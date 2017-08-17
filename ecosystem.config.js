14b09a6808c30a0c11dfb7f3f159ae3657e82a96
zhangyi221 <yid@163.com>
2017-08-17 08:31:51 +0800

    1

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,17 +1,19 @@
 {
-    "apps" : 
-    [{
-        "name": "www",
-        "script": "./bin/www",
-        "watch": true,
-        "env": {
-            "NODE_ENV": "production"
-        },
-        "env_production": {
-            "NODE_ENV": "production"
-        }
-    }, {
-        "name": "PM2-WEB",
-        "script": "pm2-web"
-    }]
+    module.exports = {
+        "apps":
+        [{
+            "name": "www",
+            "script": "./bin/www",
+            "watch": true,
+            "env": {
+                "NODE_ENV": "production"
+            },
+            "env_production": {
+                "NODE_ENV": "production"
+            }
+        }, {
+            "name": "PM2-WEB",
+            "script": "pm2-web"
+        }]
+    }
 }
